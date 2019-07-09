ef57430c1ae0a9a8f9977e377e1394a9a318978f
Robin Andersson <k.robin.andersson@gmail.com>
2019-07-09 20:38:38 +0200

    minor

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
 
 import AuthContext from './context/auth-context';
