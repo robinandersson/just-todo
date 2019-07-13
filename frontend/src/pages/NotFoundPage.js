d71a2bf17e36492eafc107e4913d99334155138d
Robin Andersson <k.robin.andersson@gmail.com>
2019-07-13 14:16:40 +0200

    style 404-page

--- frontend/src/pages/NotFoundPage.js
+++ frontend/src/pages/NotFoundPage.js
@@ -1,15 +1,22 @@
 import React from 'react';
 
+import Icon from '../components/Icon';
+
 const NotFoundPage = props => {
   return (
-    <div className="container mx-auto p-10 shadow bg-red-350 border-8 border-red-500 text-red-600">
-      <h1 className="text-2xl">404 Not Found!</h1>
-      <p className="">
-        The page you tried to visit does not exist{' '}
-        <span role="img" aria-label="screaming emoji">
-          😱
-        </span>
-      </p>
+    <div className="mx-auto shadow bg-red-400 rounded shadow-2xl w-4/6 flex flex-row p-6 text-white">
+      <div className="pr-8 pl-4 text-4xl flex content-center items-center">
+        <Icon symbol="crossCircle" />
+      </div>
+      <div>
+        <h2 className="text-2xl">404 Not Found!</h2>
+        <p>
+          The page you tried to visit does not exist{' '}
+          <span role="img" aria-label="screaming emoji">
+            😱
+          </span>
+        </p>
+      </div>
     </div>
   );
 };
