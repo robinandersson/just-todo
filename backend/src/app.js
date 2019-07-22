b5cd5f2283bd444a0ebba9dcae615576b10ee848
Robin Andersson <k.robin.andersson@gmail.com>
2019-07-22 17:20:28 +0200

    fix: further config backend and frontend for heroku development

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 
 const { graphqlRoutes } = require('./api');
 const { isAuthenticated } = require('./middleware/is-auth');
@@ -18,5 +19,21 @@ app.use((req, res, next) => {
 });
 
 app.use('/graphql', graphqlRoutes);
+
+switch (process.env.NODE_ENV) {
+  case 'production':
+    // Serve the static files from the React app
+    app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+    // Handles any requests that don't match the ones above
+    app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname + '../frontend/build/index.html'));
+    });
+    break;
+
+  case 'development':
+  default:
+    break;
+}
 
 app.listen(8000);
