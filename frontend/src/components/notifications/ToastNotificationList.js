24572ead2e1d4d0c2d05b85fdb911418b865f8b3
Robin Andersson <k.robin.andersson@gmail.com>
2019-08-23 17:53:24 +0200

    add fixedPosition prop to ToastNotificationList
    Allows to flexibly choose if it should appear at the top or at the bottom

--- frontend/src/components/notifications/ToastNotificationList.js
+++ frontend/src/components/notifications/ToastNotificationList.js
@@ -2,13 +2,21 @@ import React from 'react';
 
 import ToastNotification from './ToastNotification';
 
-const ToastNotificationList = ({ removeToast, notifications }) => {
+const ToastNotificationList = ({
+  removeToast,
+  notifications,
+  fixedPosition = 'top',
+}) => {
   if (!Array.isArray(notifications) || !notifications.length) return null;
 
   const handleRemove = i => () => removeToast(i);
 
+  const oppositePosition = fixedPosition === 'top' ? 'bottom' : 'top';
+
   return (
-    <div className="fixed top-1/24 bottom-auto left-1/12 sm:left-1/6 right-1/12 sm:right-1/6">
+    <div
+      className={`fixed ${fixedPosition}-1/24 ${oppositePosition}-auto left-1/12 sm:left-1/6 right-1/12 sm:right-1/6`}
+    >
       {notifications.map((notification, index) => {
         if (!notification) return null;
 
