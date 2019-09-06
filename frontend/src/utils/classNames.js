0a55df05075773acec751a36f686439f5efbc0b2
Robin Andersson <k.robin.andersson@gmail.com>
2019-09-06 15:42:04 +0200

    fix: correctly handle array params to concatClassNames function

--- frontend/src/utils/classNames.js
+++ frontend/src/utils/classNames.js
@@ -1,8 +1,18 @@
 const concatClassNames = (...classNames) => {
   // split classNames into arrays and concatenate
-  const cssProperties = classNames.reduce((acc, css) => {
-    // sanitize falsey values
-    if (!css) return acc;
+  const cssProperties = classNames.reduce((acc, elem) => {
+    // skip falsey values and empty array
+    if (!elem || (Array.isArray && elem.length === 0)) return acc;
+
+    const css = (() => {
+      // recursive call to handle (nested) array
+      if (Array.isArray(elem)) return concatClassNames(...elem);
+      if (typeof elem === 'string') return elem;
+
+      throw new Error(
+        `ConcatClassNames function does not accept ${typeof elem} as parameter`
+      );
+    })();
 
     // split and append css to accumulated css array
     return acc.concat(css.trim().split(' '));
