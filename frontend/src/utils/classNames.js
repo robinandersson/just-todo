fc03b598790edb53b22d93d6c86ec95d3f477fdf
Robin Andersson <k.robin.andersson@gmail.com>
2019-09-07 14:17:24 +0200

    rewrite concatClassNames functions to correctly handle falsey values

--- frontend/src/utils/classNames.js
+++ frontend/src/utils/classNames.js
@@ -1,25 +1,28 @@
 const concatClassNames = (...classNames) => {
   // split classNames into arrays and concatenate
   const cssProperties = classNames.reduce((acc, elem) => {
-    // skip falsey values and empty array
-    if (!elem || (Array.isArray && elem.length === 0)) return acc;
-
     const css = (() => {
       // recursive call to handle (nested) array
       if (Array.isArray(elem)) return concatClassNames(...elem);
-      if (typeof elem === 'string') return elem;
 
-      throw new Error(
-        `ConcatClassNames function does not accept ${typeof elem} as parameter`
-      );
+      // do not allow objects (except null) or functions, etc.
+      if (typeof elem === 'object' && elem !== null) {
+        throw new Error(
+          `ConcatClassNames function does not accept ${typeof elem} as parameter`
+        );
+      }
+      return elem;
     })();
 
-    // split and append css to accumulated css array
+    // skip falsey values and empty array
+    if (!elem || (Array.isArray && elem.length === 0)) return acc;
+
+    // element is valid css: split and append css to accumulated css array
     return acc.concat(css.trim().split(' '));
   }, []);
 
   // filter out distinct properties and create new css string
-  return [...new Set(cssProperties)].join(' ');
+  return [...new Set(cssProperties.filter(elem => elem))].join(' ');
 };
 
 export { concatClassNames };
