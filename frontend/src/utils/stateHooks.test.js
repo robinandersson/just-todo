3e2c95a5146848d9d45d2fc20b4241662850b20f
Robin Andersson <k.robin.andersson@gmail.com>
2019-09-07 18:45:43 +0200

    add tests for forceUpdate custom hook

--- frontend/src/utils/stateHooks.test.js
+++ frontend/src/utils/stateHooks.test.js
@@ -1,7 +1,7 @@
-import { usePrevious } from './stateHooks';
-
-import React from 'react';
+import React, { useRef } from 'react';
 import { mount } from 'enzyme';
+
+import { usePrevious, useForceUpdate } from './stateHooks';
 
 /* This is a basic React Component to house hook(s) for unit-testing purposes
  *
@@ -29,4 +29,38 @@ test('usePrevious returns previous value', () => {
   expect(value).toBe('initial value');
   wrapper.setProps({ value: 'third value' });
   expect(value).toBe('second value');
+});
+
+test('useForceUpdate causes rerender', () => {
+  let renderedCount = 0;
+
+  const callback = forceUpdateOnce => {
+    const rerenderDoneRef = useRef(false); // only rerender once (don't cause infinite loop)
+    const forceUpdate = useForceUpdate();
+
+    renderedCount++;
+
+    if (forceUpdateOnce && !rerenderDoneRef.current) {
+      rerenderDoneRef.current = true;
+      forceUpdate();
+    }
+  };
+
+  // test without and then with forceUpdate
+
+  // .. without forceUpdate (without rerender)
+  const wrapperWithoutRerender = mount(
+    <HookContainer callback={callback} value={false} />
+  );
+  // increments once on first render, therefore value should be 1
+  expect(renderedCount).toBe(1);
+
+  // .. with forceUpdate (with rerender)
+  renderedCount = 0;
+  const wrapperWithRerender = mount(
+    <HookContainer callback={callback} value={true} />
+  );
+
+  // increments once on first render, increments again on rerender, therefore value should be 2
+  expect(renderedCount).toBe(2);
 });
